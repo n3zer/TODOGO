e30e440fc6b2057fb17d34d4210436af3a5df212
n3z <n3z@DESKTOP-GTR4A24>
2023-02-04 23:04:17 +0600

    Add sqlite to proj, bind viewtask date in page calendar to calendar

---
+++ TODOGO/Models/DatabaseManager.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TODOGO
+{
+    public class DatabaseManager
+    {
+        private readonly SQLiteConnection _database;
+
+        public DatabaseManager()
+        {
+            _database = new SQLiteConnection("Data Source=viewmodels.db;Version=3;");
+            _database.CreateTable<TaskViewModel>();
+        }
+
+        public void SaveViewModels(List<TaskViewModel> viewModels)
+        {
+            foreach (var viewModel in viewModels)
+            {
+                _database.Insert(viewModel);
+            }
+        }
+
+        public List<TaskViewModel> LoadViewModels()
+        {
+            return _database.Table<TaskViewModel>().ToList();
+        }
+    }
+}
---
+++ TODOGO/ViewModels/CalendarPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm;
+using System;
+
+namespace TODOGO
+{
+    public class CalendarPageViewModel
+    {
+        public DateTime CurrentDate { get; set; }
+
+
+        public CalendarPageViewModel()
+        {
+            CurrentDate = DateTime.Now.Date;
+        }
+    }
+}
